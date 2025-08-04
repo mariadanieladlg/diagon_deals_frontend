47b6375cc3b5c230f558049591ba79cd9a60f102
thathsaraudari <105314224+thathsaraudari@users.noreply.github.com>
2025-08-04 23:40:26 +0200

    implemented edit button

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import AddProduct from "./pages/AddProduct";
+import UpdateProduct from "./pages/UpdateProduct"
 import "./App.css";
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/add-product" element={<AddProduct />} />
+      <Route path="/edit/:id" element={<UpdateProduct />} />
     </Routes>
   );
 }
--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import ProductCard from "./ProductCard";
+import { useNavigate } from "react-router-dom";
 
 const ProductList = ({ categoryFilter }) => { 
   const [products, setProducts] = useState([]);
@@ -33,17 +34,29 @@ const ProductList = ({ categoryFilter }) => {
       .catch((error) => console.error("Error deleting product:", error));
   };
 
+ const navigate = useNavigate();
+
+const handleEdit = (product) => {
+  navigate(`/edit/${product.id}`); 
+};
+
   return (
-    <div>
-      {products.length === 0 ? (
-        <p>No products found.</p>
-      ) : (
-        products.map((product) => (
-          <ProductCard key={product.id} product={product} onDelete={handleDelete} />
-        ))
-      )}
+  <div>
+    {products.length === 0 ? (
+      <p>No products found.</p>
+    ) : (
+      products.map((product) => (
+        <ProductCard
+          key={product.id}
+          product={product}
+          onDelete={handleDelete}
+          onEdit={handleEdit}
+        />
+      ))
+    )}
     </div>
-  );
+);
+
 };
 
 export default ProductList;
